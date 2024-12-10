9c62b8fb155b40853b85715312952b8e248a2029
zhendler <zhendlerbing@gmail.com>
2024-12-10 12:52:27 +0200

    login, logout, register, edit, create, clickable tags, authors + reset pas

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -139,6 +139,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'  # Використовуй відповідний SMTP сервер
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'zhendlerbing@gmail.com'
+EMAIL_HOST_USER = 'zhndlerbing@gmail.com'
 EMAIL_HOST_PASSWORD = 'your_password'
 
+#
